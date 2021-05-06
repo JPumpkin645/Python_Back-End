a5fbe970df32dfb2b217ba494fc1c207bc8fd483
JPumpkin645 <jpark9595@naver.com>
2021-05-06 23:22:15 +0900

    add comments

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, request
 from flask.json import JSONEncoder
 
+## Default JSON encoder는 set를 JSON으로 변환할 수 없다.
+## 그러므로 커스텀 엔코더를 사용하여 set을 list로 변환하여 JSON으로 변환 가능하게 해주어야한다.
+
+
 app          = Flask(__name__)
 app.id_count = 1
 app.users    = {}
