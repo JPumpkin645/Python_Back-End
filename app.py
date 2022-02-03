be2095bef6fad6689fa242046f69e38b3e8759b5
JPumpkin645 <jpark9595@naver.com>
2022-02-03 15:48:08 +0900

    rearrange code in app.py

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask, jsonify, request
 from flask.json import JSONEncoder
 
-## Default JSON encoder는 set를 JSON으로 변환할 수 없다.
-## 그러므로 커스텀 엔코더를 사용하여 set을 list로 변환하여 JSON으로 변환 가능하게 해주어야한다.
-
-
 app          = Flask(__name__)
-app.id_count = 1
-app.users    = {}
-app.tweets   = []
 
+###JSON Encoder####
 class CustomJSONEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, set):
@@ -19,72 +13,82 @@ class CustomJSONEncoder(JSONEncoder):
 
 app.json_encoder = CustomJSONEncoder
 
+####ping-pong test####
 @app.route("/ping", methods=['GET'])
 def ping():
     return "pong"
 
+####sign-up####
+app.users = {}
+app.id_count = 1
+
 @app.route("/sign-up", methods=['POST'])
 def sign_up():
-    new_user                = request.json
-    new_user["id"]          = app.id_count
+    new_user = request.json
+    new_user["id"] = app.id_count
     app.users[app.id_count] = new_user
-    app.id_count            = app.id_count + 1
+    app.id_count = app.id_count + 1
 
     return jsonify(new_user)
 
+####tweet#####
+app.tweets = []
 @app.route("/tweet", methods=['POST'])
 def tweet():
     payload = request.json
     user_id = int(payload['id'])
-    tweet   = payload['tweet']
+    tweet = payload['tweet']
 
     if user_id not in app.users:
-        return '사용자가 존재하지 않습니다', 400
+        return "User does not exist", 400
 
     if len(tweet) > 300:
-        return '300자를 초과했습니다', 400
+        return "Total length is over 300", 400
 
     user_id = int(payload['id'])
     
     app.tweets.append({
-        'user_id' : user_id,
-        'tweet'   : tweet
+        'user_id': user_id,
+        'tweet': tweet
     })
 
     return '', 200
 
+####follow####
 @app.route("/follow", methods=['POST'])
 def follow():
-    payload           = request.json
-    user_id           = int(payload['id'])
+    payload = request.json
+    user_id = int(payload['id'])
     user_id_to_follow = int(payload['follow'])
 
-    if user_id not in app.user or user_id_to_follow not in app.users:
-        return '사용자가 존재하지 않습니다', 400
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return "User does not exist", 400
 
     user = app.users[user_id]
     user.setdefault('follow', set()).add(user_id_to_follow)
 
     return jsonify(user)
 
+####unfollow####
 @app.route('/unfollow', methods=['POST'])
 def unfollow():
-    payload           = request.json
-    user_id           = int(payload['id'])
+    payload = request.json
+    user_id = int(payload['id'])
     user_id_to_follow = int(payload['unfollow'])
 
     if user_id not in app.users or user_id_to_follow not in app.users:
-        return '사용자가 존재하지 않습니다', 400
+        return "User does not exist", 400
 
     user = app.users[user_id]
     user.setdefault('follow', set()).discard(user_id_to_follow)
 
     return jsonify(user)
 
+####timeline####
 @app.route('/timeline/<int:user_id>', methods=['GET'])
 def timeline(user_id):
     if user_id not in app.users:
-        return '사용자가 존재하지 않습니다', 400
+        return "User does not exist", 400
 
     follow_list = app.users[user_id].get('follow', set())
     follow_list.add(user_id)
@@ -92,5 +96,5 @@ def timeline(user_id):
 
     return jsonify({
         'user_id'   : user_id,
-        'time_line' : time_line
+        'time_line' : timeline
     })
